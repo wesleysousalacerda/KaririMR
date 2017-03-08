eedaca835ca1ef4ccb76c43df0ef74196bf24ef7
mattheusvinicius <alencarferreira@live.com>
2017-03-08 10:54:59 -0300

    Limitar o escopo dos campos do Controller Usuario

--- Controller/UsuarioController.php
+++ Controller/UsuarioController.php
@@ -10,7 +10,8 @@ class UsuarioController{
     
     public function Cadastrar(Usuario $usuario) {
         if(strlen($usuario->getNome()) >= 5 && strpos($usuario->getEmail(), "@") && strpos($usuario->getEmail(), ".") &&
-                strlen($usuario->getCpf()) == 14 && $usuario->getSexo() != "" && $usuario->getPermissao() >= 1 && $usuario->getPermissao() <= 2 
+                strlen($usuario->getCpf()) == 14 && $usuario->getSexo() != "" && $usuario->getPermissao_id() >= 1 &&
+                $usuario->getLogradouro_cod() >= 1 && $usuario->getPlano_id() >= 1 && $usuario->getPermissao_id() <= 3 
                 && $usuario->getStatus() >= 1 && $usuario->getStatus() <= 2){
             
             return $this->usuarioDAO->Cadastrar($usuario);
