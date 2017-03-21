891e08a44af4698a67acbdd7bf0858e864ddac64
wesleysousalacerda <wesleysousalacerda@gmail.com>
2017-03-21 14:59:26 -0300

    Consulta de telefone
    Consulta, Alteração e Remoção de telefone

--- Controller/TelefoneController.php
+++ Controller/TelefoneController.php
@@ -6,15 +6,47 @@ class TelefoneController {
 
     private $telefoneDAO;
 
-    public function _construct() {
+    public function __construct() {
         $this->telefoneDAO = new TelefoneDAO();
     }
 
     public function Cadastrar(Telefone $telefone) {
-        if (strlen($telefone->getNumero())>5 && $telefone->getTipo()>0 && $telefone->getTipo()<=3){
-            return $this->telefoneDAO->Cadastrar($telefone);            
-        }else{
+        if (strlen($telefone->getNumero()) > 5 && $telefone->getTipo() > 0 && $telefone->getTipo() <= 3 && $telefone->getUsuario()->getCod() > 0) {
+            return $this->telefoneDAO->Cadastrar($telefone);
+        } else {
             return false;
+        }
+    }
+
+    public function Deletar(int $cod) {
+        if ($cod > 0) {
+            return $this->telefoneDAO->Deletar($cod);
+        } else {
+            return false;
+        }
+    }
+
+    public function Alterar(Telefone $telefone) {
+        if (strlen($telefone->getNumero()) > 5 && $telefone->getTipo() > 0 && $telefone->getTipo() <= 3 && $telefone->getCod() > 0) {
+            return $this->telefoneDAO->Alterar($telefone);
+        } else {
+            return false;
+        }
+    }
+
+    public function RetornarTodosUsuario(int $usuarioCod) {
+        if ($usuarioCod > 0) {
+            return $this->telefoneDAO->RetornarTodosUsuario($usuarioCod);
+        } else {
+            return null;
+        }
+    }
+
+    public function RetornarCod(int $telefoneCod) {
+        if ($telefoneCod > 0) {
+            return $this->telefoneDAO->RetornarCod($telefoneCod);
+        } else {
+            return null;
         }
     }
 
