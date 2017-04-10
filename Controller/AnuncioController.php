20a7086838c8917fb74b0dbc5c9d499f11fe4a50
wesleysousalacerda <wesleysousalacerda@gmail.com>
2017-04-10 09:42:35 -0300

    Validação de anuncios no Controller.

--- Controller/AnuncioController.php
+++ Controller/AnuncioController.php
@@ -1,8 +1,56 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once("../DAL/anuncioDAO.php");
 
+class AnuncioController {
+
+    private $anuncioDAO;
+
+    function __construct() {
+        $this->anuncioDAO = new anuncioDAO();
+    }
+
+    public function Cadastrar(Anuncio $anuncio) {
+        if (trim(strlen($anuncio->getNome())) > 0 && $anuncio->getValor() > 0 && $anuncio->getStatus() > 0 && $anuncio->getPerfil() > 0 && $anuncio->getTipo() > 0 && trim(strlen($anuncio->getDescricao())) >= 10 && $anuncio->getCategoria()->getCod() > 0 && $anuncio->getUsuario()->getCod() > 0) {
+            return $this->anuncioDAO->Cadastrar($anuncio);
+        } else {
+            return false;
+        }
+    }
+
+    public function Alterar(Anuncio $anuncio) {
+        if (trim(strlen($anuncio->getNome())) > 0 && $anuncio->getValor() > 0 && $anuncio->getStatus() > 0 && $anuncio->getPerfil() > 0 && $anuncio->getTipo() > 0 && trim(strlen($anuncio->getDescricao())) >= 10 && $anuncio->getCategoria()->getCod() > 0 && $anuncio->getCod() > 0) {
+            return $this->anuncioDAO->Alterar($anuncio);
+        } else {
+            return false;
+        }
+    }
+
+    public function RetornarTodosFiltro(string $termo, int $tipo, int $status, int $perfil, int $categoriacod) {
+
+        if (strlen($termo) > 0 && $tipo > 0 && $status > 0 && $perfil > 0 && $categoriacod > 0) {
+            return $this->anuncioDAO->RetornarTodosFiltro($termo, $tipo, $status, $perfil, $categoriacod);
+        } else {
+            return null;
+        }
+    }
+
+    public function RetornarCod(int $cod) {
+        if ($cod > 0) {
+            return $this->anuncioDAO->RetornarCod($cod);
+        } else {
+            return null;
+        }
+    }
+
+    public function RetornarCompletoCod($cod) {
+        if ($cod > 0) {
+            return $this->anuncioDAO->RetornarCompletoCod($cod);
+        } else {
+            return null;
+        }
+    }
+
+}
+
+?>
