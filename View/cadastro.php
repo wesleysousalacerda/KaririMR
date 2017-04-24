cbbb24bda9c97b0b9f0b7bfa389baea33d3a611f
mattheusvinicius <alencarferreira@live.com>
2017-04-23 22:32:20 -0300

    Validação de senha(formulario de cadastro)

--- View/cadastro.php
+++ View/cadastro.php
@@ -52,7 +52,7 @@
             </div>
 
             <div class="grid-50 mobile-grid-100 coluna">
-                <label for="txtCpf">Sexo</label>
+                <label for="txtSexo">Sexo</label>
                 <select name="slSexo" id="slSexo">
                     <option value="m">Masculino</option>
                     <option value="f">Feminino</option>
@@ -88,5 +88,54 @@
         <br />
     </div>
 </div>
+<script src="js/mask.js" type="text/javascript"></script>
+<script>
+    $('#txtCpf').mask('000.000.000-00'); //CPF
+    $('#txtData').mask('00/00/0000');
 
+    $("#txtSenha, #txtSenha2").keyup(function () {
+        var resultadoValidacao = ValidarSenha(7);
+        ExibeMensagens(resultadoValidacao);
+    });
 
+    $("#txtUsuario").keyup(function () {
+        ValidaUsuario();
+    });
+
+    $("#txtCpf").keyup(function () {
+        if (ValidaCPF($("#txtCpf").val())) {
+            $("#spValidaCpf").css("color", "#39C462");
+            $("#spValidaCpf").text("CPF válido");
+        } else {
+            $("#spValidaCpf").css("color", "#FF3730");
+            $("#spValidaCpf").text("CPF inválido");
+        }
+    });
+    function ValidarSenha(minLenght) {
+        var senha1 = document.getElementById("txtSenha").value;
+        var senha2 = document.getElementById("txtSenha2").value;
+        var valido = false;
+        if (senha1.length >= minLenght && senha2.length >= minLenght) {
+            if (senha1 === senha2) {
+                valido = true;
+            }
+        }
+
+        return valido;
+    }
+
+    function ExibeMensagens(valido) {
+        var spValidaSenha = document.getElementsByClassName("spValidaSenha");
+        if (valido) {
+            for (var i = 0; i < spValidaSenha.length; i++) {
+                spValidaSenha[i].style.color = "#39C462";
+                spValidaSenha[i].innerText = "Senhas corretas";
+            }
+        } else {
+            for (var i = 0; i < spValidaSenha.length; i++) {
+                spValidaSenha[i].style.color = "#FF3730";
+                spValidaSenha[i].innerText = "Senhas inválidas";
+            }
+        }
+    }
+</script>
