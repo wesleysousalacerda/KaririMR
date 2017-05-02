e5bfa086fc1e8d072e77d023411987012f9f376b
mattheusvinicius <alencarferreira@live.com>
2017-05-02 17:42:59 -0300

    form conato e atual. quem somos

--- View/contato.php
+++ View/contato.php
@@ -1,7 +1,17 @@
 <div id="dvContato">
     <h1>Contato</h1>
     <br />
-    <div>
-        Conteúdo
-    </div>
+    <div id="dvFormContato" class="formcontroles">
+        <!--1º row-->
+        <div class="linha">
+            <div class="grid-50 mobile-grid-100 coluna">
+                <label for="txtNome">Nome</label>
+                <input type="text" id="txtNome" name="txtNome" placeholder="Nome completo" />
+            </div>
+
+            <div class="grid-50 mobile-grid-100 coluna">
+                <label for="txtEmail">E-mail  <span class="spValidaEmail"></span></label>
+                <input type="text" id="txtEmail" name="txtEmail" placeholder="email@dominio.com" />
+            </div>
+        </div>
 </div>
--- View/quemsomos.php
+++ View/quemsomos.php
@@ -4,30 +4,45 @@
     <div>
         <p align="justify">O KARIRI MR. Começou a ser planejado em 2015, com o intuito de facilitar as pessoas que trabalham com venda de veículo ou que estivesse interesse em comprar um, na região do Cariri, no Sul do estado do Ceara. Com o objetivo de juntar em uma única plataforma.</p>
         <br>
-        
+
         <p align="justify">Num mercado em que a maioria da concorrência se limita a revender em um local físico, a solução foi criar um ambiente web com igualdade para todos. Um dos nossos principais diferenciais é a possibilidade de escolher planos diferentes do sistema, o que dará prioridade nos anúncios, ficando à frente nas buscas.</p>
         <br>
-        
+
         <p align="justify">O KARIRI MR foi desenvolvido por uma equipe de cinco jovens universitários da UNILEÂO, do curso de Análise e desenvolvimento de sistemas, com o objetivo de obtenção de nota nas disciplinas Projeto Integrador I, II, III e IV.</p>
         <br>
-        
+
         <h4><i>Missão</i></h4>
         <br>
-        
+
         <p align="justify">“Criar para o público em geral, soluções de comprar e anunciar seu veículo, de maneira transparente, preservando uma concorrência justa dentro dos padrões da ética, dando oportunidade a todos, e garantindo lucro para o negócio”.</p>
         <br>
-        
+
         <h4><i>Visão</i></h4>
         <br>
-        
+
         <p align="justify">“Ser referência no mercado Caririense em serviços de anúncios automotivos focados no público geral”.</p>
         <br>
-        
+
         <h4><i>Valores</i></h4>
         <br>
-        
+
         <p align="justify">Comprometimento, Superação de Resultados, Melhoria contínua, Inovação, Sustentabilidade.</p>
         <br>
+
+        <h4 align="center">Integrantes</h4>
+        <br>
+
+        <img class="img-circle" src="https://lh4.googleusercontent.com/-tP_RkLEB-aI/AAAAAAAAAAI/AAAAAAAALW8/fRalgss8M40/photo.jpg?sz=328" width="130" height="130" title="Progamador Master">
+
+        <img class="img-circle" src="https://scontent.frec1-2.fna.fbcdn.net/v/t1.0-0/p206x206/12522988_904746512965785_1724825347877146286_n.jpg?oh=df3ea7f8a4594901b75e9e5945fadd17&oe=598AEA67" width="130" height="130" >
+
+        <img class="img-circle" src="https://scontent.frec1-2.fna.fbcdn.net/v/t1.0-9/1000863_412420052237003_969414263_n.jpg?oh=6bbda3f538b69a956da7487709ced99b&oe=59839B44" width="130" height="130" >
+
+        <img class="img-circle" src="https://scontent.frec1-2.fna.fbcdn.net/v/t1.0-9/531382_127954834035772_781588132_n.jpg?oh=70881ac5a4c3135a8d23f4ac747daaa5&oe=5978FE10" width="130" height="130" >
+
+        <img class="img-circle" src="https://scontent.frec1-2.fna.fbcdn.net/v/t1.0-9/1454619_389074317892327_1226019537_n.jpg?oh=23a10e3fe6c7031f231e308eebb6919a&oe=5983F4B6" width="130" height="130" >
+        <br>
+        
         
     </div>
 </div>
