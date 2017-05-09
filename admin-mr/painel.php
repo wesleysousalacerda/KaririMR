3e87f933d363d04c8d58fd6e7ff3346493a5bf37
wesleysousalacerda <wesleysousalacerda@gmail.com>
2017-05-09 10:00:30 -0300

    layout otimização

--- admin-mr/painel.php
+++ admin-mr/painel.php
@@ -11,98 +11,101 @@ if (isset($_SESSION["logado"])) {
 ?>
 <!doctype html>
 <html lang="pt-br">
-    <head>
-        <title>Kariri Multi Revendedoras - Painel</title>
-        <meta charset="utf-8" />
-        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script src="../js/jquery-3.1.1.min.js" type="text/javascript"></script>
-        <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
-        <link rel="shortcut icon" href="../img/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <script src="js/script.js" type="text/javascript"></script>
-    </head>
-    <body>
-        <div id="dvConteudoPrincipal">
-            <div class="row" id="dvTopo">
-                <div class="col-xs-12 hidden-lg text-center">
-                    <div class="dvlogoTopo">
-                        <span class="glyphicon glyphicon-menu-hamburger btn btn-default btn-lg" aria-hidden="true" id="btnMenuResponsive"></span>
-                        <a href="painel.php"><img src="../img/logoKaririmr.png" style="width: 550px;" alt="Kariri Multi Revendedoras" /></a>
-                    </div>
-                </div>
-                <div class="col-xs-12 hidden-xs">
-                    <div class="dvlogoTopo">
-                        <a href="painel.php"><img src="../img/logoKaririmr.png" style="width: 550px;" alt="Kariri Multi Revendedoras" /></a>
-                    </div>
+<head>
+    <title>Kariri Multi Revendedoras - Painel</title>
+    <meta charset="utf-8" />
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <script src="../js/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" href="../img/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="js/script.js" type="text/javascript"></script>
+</head>
+<body>
+    <div id="dvConteudoPrincipal">
+        <div class="row" id="dvTopo">
+            <div class="col-xs-12 hidden-lg text-center">
+                <div class="dvlogoTopo">
+                    <span class="glyphicon glyphicon-menu-hamburger btn btn-default btn-lg" aria-hidden="true" id="btnMenuResponsive"></span>
+                    <a href="painel.php"><img src="../img/logoKaririmr.png" style="width: 550px;" alt="Kariri Multi Revendedoras" /></a>
                 </div>
             </div>
-            <div class="row" id="dvMenuResponsive" style="display: none;">
-                <div class="col-xs-12">
-                    <ul id="ulMenuResponsive">
+            <div class="col-xs-12 hidden-xs">
+                <div class="dvlogoTopo">
+                    <a href="painel.php"><img src="../img/logoKaririmr.png" style="width: 550px;" alt="Kariri Multi Revendedoras" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="row" id="dvMenuResponsive" style="display: none;">
+            <div class="col-xs-12">
+                <ul id="ulMenuResponsive">
+                    <li class="firstLine"><a href="painel.php">Inicio</a></li>
+                    <li><a href="?pagina=usuario">Usuário</a></li>
+                    <li><a href="?pagina=anuncio">Anuncios</a></li>
+                    <li><a href="?pagina=categoria">Categoria</a></li>
+                    <li><a href="?pagina=subcategoria">Subcategoria</a></li>
+                    <li><a href="?pagina=contato">Contato</a></li>
+                    <li><a href="logout.php">Sair</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row no-gutter">
+            <div class="col-lg-2 gridLeft hidden-xs" id="dvEsquerda">
+                <div id="dvMenuLateral">
+                    <ul id="ulMenu">
                         <li class="firstLine"><a href="painel.php">Inicio</a></li>
                         <li><a href="?pagina=usuario">Usuário</a></li>
-                        <li><a href="?pagina=anuncio">Anuncios</a></li>
+                        <li><a href="?pagina=anuncio">Anúncios</a></li>
+                        <li><a href="?pagina=automovel">Automóveis</a></li>
                         <li><a href="?pagina=categoria">Categoria</a></li>
                         <li><a href="?pagina=subcategoria">Subcategoria</a></li>
                         <li><a href="?pagina=contato">Contato</a></li>
                         <li><a href="logout.php">Sair</a></li>
                     </ul>
-                </div>
-            </div>
-            <div class="row no-gutter">
-                <div class="col-lg-2 gridLeft hidden-xs" id="dvEsquerda">
-                    <div id="dvMenuLateral">
-                        <ul id="ulMenu">
-                            <li class="firstLine"><a href="painel.php">Inicio</a></li>
-                            <li><a href="?pagina=usuario">Usuário</a></li>
-                            <li><a href="?pagina=anuncio">Anúncios</a></li>
-                            <li><a href="?pagina=automovel">Automóveis</a></li>
-                            <li><a href="?pagina=categoria">Categoria</a></li>
-                            <li><a href="?pagina=subcategoria">Subcategoria</a></li>
-                            <li><a href="?pagina=contato">Contato</a></li>
-                            <li><a href="logout.php">Sair</a></li>
-                        </ul>
-                    </div>
+
                 </div>
 
-                <div class="col-xs-12 col-lg-10" id="dvDireita">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <?php
-                            require_once("../Util/RequestPage.php");
-                            ?>
-                            <div class="clear"></div>
-                        </div>
-                    </div>
-                     <div class="row" id="dvRodape">
-                        <div class="col-lg-6 col-xs-12 alignCenter">
-                            <br /><br />
-                            <p>&copy; Kariri Multi Revendedoras- Todos os Direitos Reservados</p>  
-                        </div>
-
-                        <div class="col-lg-6 col-xs-12 alignCenter">
-                            <a href="#">Facebook</a><br  />
-                            <a href="#">Twitter</a><br  />
-                            <a href="#">Youtube</a><br  />
-                            <a href="#">Instagram</a><br  />
-                        </div>
-                    </div>
-                    
-                    <!--Aqui termina o conteúdo da direita-->
-                   
-                </div>
-               
             </div>
-            
+
+            <div class="col-xs-12 col-lg-10" id="dvDireita">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <?php
+                        require_once("../Util/RequestPage.php");
+                        ?>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <div class="row" id="dvRodape">
+                    <div class="col-lg-6 col-xs-12 alignCenter">
+                        <p style="margin-top: 25px">&copy; Kariri Multi Revendedoras- Todos os Direitos Reservados</p>  
+                    </div>
+                    <div class="col-lg-3 col-xs-12 alignCenter">
+                        <div id="iconesSociais">
+                            <a href=""><img src="http://<?php echo $_SERVER['HTTP_HOST']?>/img/social/facebook.png" alt=""/></a>
+                            <a href=""><img src="http://<?php echo $_SERVER['HTTP_HOST']?>/img/social/twitter.png" alt=""/></a>
+                            <a href=""><img src="http://<?php echo $_SERVER['HTTP_HOST']?>/img/social/youtube.png" alt=""/></a>
+                            <a href=""><img src="http://<?php echo $_SERVER['HTTP_HOST']?>/img/social/instagram.png" alt=""/></a>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+
+            <!--Aqui termina o conteúdo da direita-->
+
         </div>
-        <script>
-            $(document).ready(function () {
-                $("#btnMenuResponsive").click(function () {
-                    $("#dvMenuResponsive").slideToggle("slow");
-                });
+
+
+    </div>
+    
+    <script>
+        $(document).ready(function () {
+            $("#btnMenuResponsive").click(function () {
+                $("#dvMenuResponsive").slideToggle("slow");
             });
-        </script>
-                </body>
-                </html>
+        });
+    </script>
+</body>
+</html>
